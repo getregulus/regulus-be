8ecb31d78871bea0e94a402cf911ea03ed45a994
aylin.gorgun <aylingrgn2@gmail.com>
2025-01-10 01:09:47 +0300

    ssl production condition

--- src/models/db.js
+++ src/models/db.js
@@ -1,11 +1,16 @@
 const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: isProduction
+  ? { rejectUnauthorized: false }
+  : false,
 });
 
 module.exports = {
