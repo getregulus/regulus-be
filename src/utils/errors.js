e22cc27995e4ea6bc3743dfa2fa2e865464ffd9f
aylingorgun <aylingrgn2@gmail.com>
2025-01-19 15:52:46 +0300

    Enhance error handling utilities for better error management

---
+++ src/utils/errors.js
@@ -0,0 +1,47 @@
+class AppError extends Error {
+  constructor(message, status = 500) {
+    super(message);
+    this.name = this.constructor.name;
+    this.status = status;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class ValidationError extends AppError {
+  constructor(message) {
+    super(message, 400);
+  }
+}
+
+class AuthenticationError extends AppError {
+  constructor(message = "Not authenticated") {
+    super(message, 401);
+  }
+}
+
+class AuthorizationError extends AppError {
+  constructor(message = "Not authorized") {
+    super(message, 403);
+  }
+}
+
+class NotFoundError extends AppError {
+  constructor(message = "Resource not found") {
+    super(message, 404);
+  }
+}
+
+class ConflictError extends AppError {
+  constructor(message = "Resource already exists") {
+    super(message, 409);
+  }
+}
+
+module.exports = {
+  AppError,
+  ValidationError,
+  AuthenticationError,
+  AuthorizationError,
+  NotFoundError,
+  ConflictError,
+};
