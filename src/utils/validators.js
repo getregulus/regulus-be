f3333b9c381568f782e79f3c13a2463d47e440e7
aylingorgun <aylingrgn2@gmail.com>
2025-10-30 00:12:32 +0300

    password validator update.

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 // Common validation patterns
 const patterns = {
   password:
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/,
   phone: /^\+?[\d\s-]{8,}$/,
   uuid: /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
 };
